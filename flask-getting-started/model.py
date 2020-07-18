521ccc4578d99b6aa98fb97b0c935211e86015cd
DESKTOP-IS0GHQ8\micro <499361328@qq.com>
2020-07-18 17:28:47 +0800

    Flask: Getting Started

---
+++ flask-getting-started/model.py
@@ -0,0 +1,14 @@
+import json
+
+
+def load_db():
+    with open('flashcards_db.json', encoding='utf-8') as f:
+        return json.load(f)
+
+
+def save_db():
+    with open('flashcards_db.json', mode='w', encoding='utf-8') as f:
+        return json.dump(db, f)
+
+
+db = load_db()
